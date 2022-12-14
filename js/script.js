b1bfbab562a74115aad5d0ad10396d884351c53a
giuseppemaisto <gmaisto98@gmail.com>
2022-12-14 15:46:44 +0100

    definito array di oggetti

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const images = [
+    {
+        image: './img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morales',
+        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+    },
+    {
+        image: './img/02.webp',
+        title: 'Ratchet & Clark: rift Apart',
+        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+    },
+    {
+        image: './img/03.webp',
+        title: 'Fortnite',
+        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+    },
+    {
+        image: './img/04.webp',
+        title: 'Stray',
+        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+    },
+    {
+        image: './img/05.webp',
+        title: 'Avengers',
+        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+    },
+
+
+]
