ec9279f5fcc041e26c77f652419ce696019b4997
giuseppemaisto <gmaisto98@gmail.com>
2022-12-14 21:33:10 +0100

    milestone 0

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
-const images = [
-    {
-        image: './img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morales',
-        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+const imagesArray = [
+    {   image:"01.webp",
+        titolo:"Marvel\'s Spiderman Miles Morales",
+        descrizione:"Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman",
+       
     },
     {
-        image: './img/02.webp',
-        title: 'Ratchet & Clark: rift Apart',
-        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+        image:"02.webp",
+        titolo:"Rachet & Clark: rift apart",
+        descrizione:"Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman",
     },
     {
-        image: './img/03.webp',
-        title: 'Fortnite',
-        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+        image:"03.webp",
+        titolo:"Fortnite",
+        descrizione:"Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman",
     },
     {
-        image: './img/04.webp',
-        title: 'Stray',
-        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+        image:"04.webp",
+        titolo:"Stray",
+        descrizione:"Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman",
     },
     {
-        image: './img/05.webp',
-        title: 'Avengers',
-        text: 'Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman'
+        image:"05.webp",
+        titolo:"Avengers",
+        descrizione:"Experience the rise of Miles Morales as the new hero masters inncredible, explosive new powers to become his own spiderman",
     },
-
 
 ]
+
+//milestone 0
+//Creiamo dinamicamente i div con le immagini del carosello
+let itemsContent = '';
+
+for(let i = 0; i<imagesArray.length; i++){
+    itemsContent += `<div class="item">
+    <img src="./img/${imagesArray[i].image}">
+    <div>
+    <h3>${imagesArray[i].titolo}</h3>
+    <p>${imagesArray[i].descrizione}</p>
+    </div>
+    </div>`
+
+    
+}
+
+const itemSlider = document.querySelector('.item-slider');
+itemSlider.innerHTML += itemsContent; 
+
+const items = document.getElementsByClassName('item');
+let itemActive = 0;
+
+items[itemActive].classList.add('active');
+
+const circle = document.getElementsByClassName('circle');
+circle[itemActive].classList.add('circle');
